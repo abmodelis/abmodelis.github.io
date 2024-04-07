796dce0c75e5c31a5134cb5883b60f55aa322533
Willy Samuel Paz Colque <willypaz243@gmail.com>
2024-04-06 20:44:13 -0400

    fix: restore course create button

--- src/views/teachers/courses.tsx
+++ src/views/teachers/courses.tsx
@@ -1,7 +1,19 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Breadcrumbs,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { CoursesService } from "../../services";
 import { Course } from "../../types";
+import { CreateDataCurses } from "components/modals/CreateDataCourses";
 
 function Courses() {
   const [loading, setLoading] = useState(true);
@@ -17,7 +29,15 @@ function Courses() {
 
   return (
     <>
-      <h1>Courses</h1>
+      <Box display="flex" sx={{ mt: 3 }}>
+        <Breadcrumbs sx={{ flexGrow: 1 }} aria-label="breadcrumb">
+          <Link underline="hover" color="inherit" href="/">
+            Panel
+          </Link>
+          <Typography color="text.primary">Breadcrumbs</Typography>
+        </Breadcrumbs>
+        <CreateDataCurses />
+      </Box>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
         {corses.map((course) => (
           <Grid item xs={2} sm={4} md={4} key={course.id}>
