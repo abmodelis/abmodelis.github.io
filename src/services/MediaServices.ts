c05e9821f4cd5fe74473054ab8866628a3a08852
Willy Samuel Paz Colque <willypaz243@gmail.com>
2024-04-12 23:21:00 -0400

    fix: change images url to https

--- src/services/MediaServices.ts
+++ src/services/MediaServices.ts
@@ -1,6 +1,7 @@
 export class MediaServices {
   static async getImage(path: string) {
     if (!path.startsWith("http")) return null;
+    path = path.replace("http", "https");
     try {
       const response = await fetch(path);
       const fileName = path.split("/").pop();
