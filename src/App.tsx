d94ef2cc79e8eddd03c44492b207472bc8ec5b1b
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2024-04-20 23:26:10 -0400

    Se quito el espacio en HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <Route path="/teachers/*" element={<Teachers />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
-    </HashRouter>  
+    </HashRouter>
   );
 }
 
