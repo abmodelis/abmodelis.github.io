a9b08627f2aefe71b0b4bdd118b37f44d6cd0b53
Anayeli Quispe <anayeliquispe222@gmail.com>
2024-04-25 11:24:45 -0400

    Revert "fix: change images url to https"
    This reverts commit c05e9821f4cd5fe74473054ab8866628a3a08852.

--- src/services/MediaServices.ts
+++ src/services/MediaServices.ts
@@ -1,7 +1,6 @@
 export class MediaServices {
   static async getImage(path: string) {
     if (!path.startsWith("http")) return null;
-    path = path.replace("http", "https");
     try {
       const response = await fetch(path);
       const fileName = path.split("/").pop();
