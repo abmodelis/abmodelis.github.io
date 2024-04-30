68f561162fe051529ccc1d3f6d024b549453267f
Josue <josuesuarezzabala@gmail.com>
2024-04-29 21:38:41 -0400

    Resolviendo conflictos2

--- src/components/sections/SectionForm.tsx
+++ src/components/sections/SectionForm.tsx
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-import {
-    Button,
-    Grid,
-    //Box,
-    TextField,
-  } from "@mui/material";
-  import { useEffect, /*useState*/ } from "react";
-  import { useForm } from "react-hook-form";
-  
-  import { Course, ICourseInput} from "types";
-  
-  type Props = {
-    course?: Course;
-    onFormSubmit: (data: ICourseInput) => void;
-    onCancel: () => void;
-    title: string;
-  };
-  
-  export const SectionForm: React.FC<Props> = ({ course, /*onFormSubmit*/ onCancel, title }) => {
-    const form = useForm<ICourseInput>();
-  
-    useEffect(() => {
-      if (!course) return;
-      form.reset({
-        title: course.title,
-      });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [course]);
-  
-    const handleSubmit = (/*data ICourseInput*/) => {
-      
-    };
-  
-    return (
-      <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <TextField
-          {...form.register("title", {
-            required: "Este campo es requerido",
-            minLength: { value: 5, message: "Minimo 5 caracteres" },
-            maxLength: { value: 60, message: "Maximo 60 caracteres" },
-            pattern: { value: /^(?=.*[A-Za-z0-9ñÑáéíóúÁÉÍÓÚ])[A-Za-z0-9ñÑáéíóúÁÉÍÓÚ\s]{5,60}$/, message: "Solo se aceptan letras y numeros" },
-          })}
-          label={title}
-          fullWidth
-          sx={{ mt: 2, mb: 1.5 }}
-          error={!!form.formState.errors.title}
-          helperText={form.formState.errors.title?.message}
-          inputProps={{ maxLength: 60 }} // Limitar la longitud máxima a 60 caracteres
-        />
-        
-          
-        <Grid container>
-          <Grid item xs={12} sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <Grid item sx={{ ml: 26 }}>
-              <Button onClick={onCancel}>Cancelar</Button>
-            </Grid>
-            <Grid item sx={{ ml: 2 }}>
-              <Button type="submit" variant="contained" color="success">
-                Guardar
-              </Button>
-            </Grid>
-          </Grid>
-           
-        </Grid>
-      </form>
-    );
-  };
-  
-=======
 import { Button, Grid, TextField } from "@mui/material";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
@@ -126,4 +56,3 @@ export const SectionForm: React.FC<Props> = ({ course, onCancel, title }) => {
     </form>
   );
 };
->>>>>>> 79b12ee080b1ef8374457438ed105ba9d96abfd4
